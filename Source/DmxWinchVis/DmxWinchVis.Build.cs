26c3999eee234290cbb9e9fff6f0454c7619e3db
WorHyako <worhyako@gmail.com>
2023-01-06 13:00:54 +0300

    base pro init

---
+++ Source/DmxWinchVis/DmxWinchVis.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DmxWinchVis : ModuleRules
+{
+	public DmxWinchVis(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new [] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
---
+++ Source/DmxWinchVis/DmxWinchVisGameModeBase.h
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DmxWinchVisGameModeBase.generated.h"
+
+UCLASS()
+class DMXWINCHVIS_API ADmxWinchVisGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
